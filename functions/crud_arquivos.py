b316b69c9a5c90f1820289932e4d5015c9d8173c
Alberto Rodrigues <allio.professional@gmail.com>
2025-02-24 20:37:03 -0300

    feat: creating base functions

---
+++ functions/crud_arquivos.py
@@ -0,0 +1,64 @@
+import os
+import random
+import struct
+
+#Criar
+def criar_nome_tam(nome: str, tam: int):
+    if tam <= 0:
+        print("Erro: 'tam' deve ser maior que zero.")
+        return
+    
+    nome_arquivo = nome + ".bin"
+    os.makedirs("directory", exist_ok=True)
+    path = os.path.join("directory", nome_arquivo)
+    if(os.path.exists(path)):
+        print("Erro: o arquivo já existe!")
+        return
+    numeros = [random.randint(0, 2**31 - 1) for _ in range(tam)]
+
+    try:
+        with open(path, "wb") as f:
+                for num in numeros:
+                    f.write(struct.pack("I", num))
+        print(f"Arquivo '{nome_arquivo}' criado com {tam} números.")
+
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+    
+#Ler
+def ler_arquivo_bin(nome: str):
+    nome_arquivo = nome + ".bin"
+    numeros = []
+
+    try:
+        with open(nome_arquivo, "rb") as f:
+            while True:
+                dados = f.read(4)  
+                if not dados:
+                    break  
+                numero = struct.unpack("I", dados)[0] 
+                numeros.append(numero)
+    except FileNotFoundError:
+        print(f"Erro: Arquivo '{nome_arquivo}' não encontrado.")
+        return None
+
+    return numeros
+
+#Deletar
+def apagar_nome(nome: str):
+    nome_arquivo = nome + ".bin"
+    try:
+        os.remove(nome_arquivo)
+        print(f"Arquivo '{nome_arquivo}' deletado com sucesso.")
+    except FileNotFoundError:
+        print(f"Erro: Arquivo {nome_arquivo} não encontrado")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+
+def arquivo_bytes_tam(nome: str, dir: str):
+    path = os.path.join(dir, nome)
+    try:
+        tamanho = os.path.getsize(path)
+        return tamanho
+    except Exception as e:
+        print(f"Erro: {e}.")
---
+++ functions/directory.py
@@ -0,0 +1,11 @@
+import os
+from methods.crud_arquivos import arquivo_bytes_tam
+
+def listar():
+    # Listar arquivos e pastas
+    arquivos = os.listdir("directory")
+
+    print("Arquivos encontrados:")
+    for arquivo in arquivos:
+        tam = arquivo_bytes_tam(arquivo, "directory")
+        print(f"{arquivo} | {tam} bytes")
