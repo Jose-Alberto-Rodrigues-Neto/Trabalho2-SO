d48256d48643a5eccb155b748337600fbf2a17b0
Alberto Rodrigues <allio.professional@gmail.com>
2025-02-24 21:18:50 -0300

    feat: function list files test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functions.crud_arquivos import criar_nome_tam, ler_arquivo_bin, apagar_nome
 from functions.directory import listar
 
-criar_nome_tam("teste9", 100000)
+criar_nome_tam("teste", 1000)
 listar()
