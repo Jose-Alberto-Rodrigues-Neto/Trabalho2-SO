742ff308d2ac224814715e136f56448132c34964
Alberto Rodrigues <allio.professional@gmail.com>
2025-02-25 17:32:11 -0300

    feat: creating disk_utils.py

---
+++ functions/disk_utils.py
@@ -0,0 +1,5 @@
+# Definições do disco virtual
+DISK_NAME = "virtual_disk.img"
+DISK_SIZE = 1 * 1024 * 1024 * 1024  # 1GB
+MOUNT_POINT = "mnt_virtual"
+LOOP_DEVICE = "/dev/loop0"  # Dispositivo de loop fixo
