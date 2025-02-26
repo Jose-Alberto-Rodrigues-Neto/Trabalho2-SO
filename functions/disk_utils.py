d997d8a8033928c8912abd5d1ea83bb73ef894e4
Alberto Rodrigues <allio.professional@gmail.com>
2025-02-26 15:24:02 -0300

    feat: addin block size

--- functions/disk_utils.py
+++ functions/disk_utils.py
@@ -1,5 +1,7 @@
 # Definições do disco virtual
 DISK_NAME = "virtual_disk.img"
-DISK_SIZE = 1 * 1024 * 1024 * 1024  # 1GB
+DISK_SIZE = 1 * 1024 * 1024 * 1024  # 1 GB
 MOUNT_POINT = "mnt_virtual"
 LOOP_DEVICE = "/dev/loop0"  # Dispositivo de loop fixo
+BLOCK_SIZE = 4096
+NUM_TAM_LIMIT = BLOCK_SIZE // 4
