5caeb7782aa0140a44cc61499d6fef91dac71290
Alberto Rodrigues <allio.professional@gmail.com>
2025-02-27 14:47:11 -0300

    fix: adding num size

--- functions/disk_utils.py
+++ functions/disk_utils.py
@@ -4,3 +4,4 @@ DISK_SIZE = 1 * 1024 * 1024 * 1024  # 1 GB
 HUGE_PAGE_SIZE = 2 * 1024 * 1024    # 2 MB
 MOUNT_POINT = "mnt_virtual"
 LOOP_DEVICE = "/dev/loop0"          # Dispositivo de loop fixo
+NUM_SIZE = 4
